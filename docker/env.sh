a6c208db4909a433b3badc09590f89347ec84c28
renanjpaula <renanjohannsen@hotmail.com>
2016-04-05 23:48:16 -0300

    Adição dos docker files

---
+++ docker/env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export DB_ADMIN_NAME=admin
+export DB_ADMIN_PASS=admin
+export DB_NAME=we-love-dev
+export DB_USER=we-love-dev
+export DB_PASS=we-love-dev
--- docker/rebuild.sh
+++ docker/rebuild.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-DB_ADMIN_NAME=admin \
-DB_ADMIN_PASS=admin \
-DB_NAME=we-love-dev \
-DB_USER=we-love-dev \
-DB_PASS=we-love-dev \
+source env.sh
 docker-compose build we-love-dev-web && docker-compose up -d
